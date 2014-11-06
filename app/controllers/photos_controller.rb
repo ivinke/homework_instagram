52b09e250e5e7b7234f0f4e747d91f911c2a053d
ivinke <ivinke@chicagobooth.edu>
2014-11-05 20:47:15 -0600

    Homework Done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,4 +9,36 @@ class PhotosController < ApplicationController
        @a = z.source
        @b = z.caption
   end
+
+  def new_form
+  end
+
+  def create_row
+    d = Photo.new
+    d.source = params[:the_source]
+    d.caption = params[:the_caption]
+    d.save
+  end
+
+  def destroy
+    @e = params[:id]
+    f = Photo.find_by({:id => @e })
+    f.destroy
+  end
+
+  def edit_form
+    @g = params[:id]
+    h = Photo.find_by({:id => @g })
+    @i = h.source
+    @j = h.caption
+  end
+
+  def update
+    @k = params[:id]
+    l = Photo.find_by({:id => @k })
+    l.source = params[:the_source]
+    l.caption = params[:the_caption]
+    l.save
+  end
+
 end
