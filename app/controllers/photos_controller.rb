2a8087fc7fee53bedae55be20495f86a429aac65
ivinke <ivinke@chicagobooth.edu>
2014-11-05 19:17:41 -0600

    1st Part Done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,10 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
-
+  def show
+      @c = params[:id]
+       z = Photo.find_by({:id => @c })
+       @a = z.source
+       @b = z.caption
+  end
 end
